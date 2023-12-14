28597654321aec846987b7e4648d2d59bda416b3
Shacham-R <39585318+Shacham-R@users.noreply.github.com>
2023-12-14 11:18:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with conn.open("fake_news_model/tokenizer.pickle","rb") as tok_file:
 
 with conn.open("fake_news_model/SNN_fake_news.keras", "rb") as model_file:
     with h5py.File(model_file, 'r') as model_gcs:
-        model = keras.models.load_model(model_gcs)
+        model = keras.saving.load_model(model_gcs)
 
 #nltk packages
 nltk.download('stopwords')
